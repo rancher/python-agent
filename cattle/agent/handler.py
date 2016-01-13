0ce1a36a0e64d7dc1a43bfee4974ab860d18a6cb
Craig Jellick <craig@rancher.com>
2016-01-13 12:34:37 -0700

    Revert "Dont delete volumes when deleting container"
    This reverts commit 80001ccfe3beace65c1874c01278bffdb7b30de9.

--- cattle/agent/handler.py
+++ cattle/agent/handler.py
@@ -105,6 +105,8 @@ class BaseHandler(object):
 class KindBasedMixin(object):
     CHECK_PATHS = [
         ["imageStoragePoolMap", "storagePool", "kind"],
+        ["volumeStoragePoolMap", "storagePool", "kind"],
+        ["snapshotStoragePoolMap", "storagePool", "kind"],
         ["instanceHostMap", "host", "kind"],
         ["instanceForceStop", "kind"],
         ["instanceInspect", "kind"],
